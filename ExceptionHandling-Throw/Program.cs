9e72257484e5e36f02416da9bb2c4186afd6ada8
abhay <abhay@DESKTOP-2G49AHH>
2022-05-02 16:31:54 +0530

    Add project files.

---
+++ ExceptionHandling-Throw/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling_Throw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please entre the first digit");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please entre the second digit");
+
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre the operation you want to perform 1. + 2. - 3. * 4. / 5. %");
+
+            string s = Console.ReadLine();
+
+            if(s == "+")
+                Console.WriteLine("The result is {0} ", a + b);
+            else if(s=="-")
+                  Console.WriteLine("The result is {0} ", a - b);
+            else if (s == "*")
+                Console.WriteLine("The result is {0} ", a * b);
+            else if (s == "/")
+            {
+                try
+                {
+                    Console.WriteLine("The result is {0} ", a / b);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Please entre valid digit");
+                }
+            }
+               
+            else if (s == "%")
+                Console.WriteLine("The result is {0} ", a % b);
+            else
+                 
+                Console.WriteLine("The result not is {0} ");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTRE THE NUMBERS");
+
+          
+
+            try
+            {
+
+                int a = int.Parse(Console.ReadLine());
+
+                int b = Convert.ToInt32(Console.ReadLine());
+
+
+                int c = a / b;
+
+                Console.WriteLine("The result is  {0} ", c);
+
+
+                //Console.WriteLine("Thanks for using me");
+
+            }
+            catch (DivideByZeroException Ex)
+            {
+                Console.WriteLine("Your input is invalid{0} ", Ex);
+
+                //Console.WriteLine("Thanks for using me");
+            }
+
+
+            catch (FormatException Ex)
+            {
+                Console.WriteLine("Your input is invalid" + Ex);
+
+                // Console.WriteLine("Thanks for using me");
+            }
+
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Your input is invalid");
+
+                //Console.WriteLine("Thanks for using me");
+            }
+            finally          // use to print deafult message 
+            {
+
+                Console.WriteLine("Thanks for using me");
+            }
+
+        
+
+
+            Console.ReadLine();
+
+
+                
+            
+
+        }
+    }
+}
